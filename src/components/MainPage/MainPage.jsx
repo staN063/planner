250ef3fb31f393f496f354df0f52ecc990e0c7b4
staN063 <ssttaann163@gmail.com>
2022-03-29 21:35:36 +0400

    Add Grid

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -4,19 +4,25 @@ import Lists from '../Lists/Lists';
 import Sports from '../Sports/Sports';
 import Report from '../Report/Report';
 
-import { Button } from 'react-bootstrap'; 
+import { Button,Container,Row,Col } from 'react-bootstrap'; 
 import './MainPage.scss';
 
 
 const MainPage = () => {
   return (
     <>
-    <Button className="App" >Hey i'm Bootstrap</Button>
-      <Calendar />
-      <Lists />
-      <Sports />
-      <Report />
-      <h3>Hello World!!</h3>
+    <Container>
+  <Row className='Row-Height '>
+    <Col md={{   offset: 3,span: 6 }} className='Border'><Calendar /></Col>
+    
+  </Row>
+  <Row>
+    <Col className='Border'><Lists /></Col>
+    <Col className='Border'> <Sports /></Col>
+    <Col className='Border'><Report /></Col>
+  </Row>
+</Container>
+ 
     </>
   );
 };
